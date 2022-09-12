4567e0c3658f87237beebc2f4996066eda5bb766
yuriynemcikov <yuriy1993@inbox.ru>
2022-09-12 10:37:30 +0300

    Task2 is done

--- HomeWork1/Task2.java
+++ HomeWork1/Task2.java
@@ -1,7 +1,14 @@
 public class Task2 {
 
+    // 1. ArrayIndexOutOfBoundsException - при переборе по столбцам.
+    // 2. NumberFormatException - если при инициализации массива неверно прописаны числа.
+    // 3. NullPointerException - если задан массив null.
+
     public static void main(String[] args) {
-        String[][] str = {{"1","2","3"},{"1","2","3"},{"1","2","3"},{"1","2","3"},{"1","2","3"},{"1","2","3"}};
+        String[][] str = {
+                {"1","2","3"},
+                {"1","2","3"},
+                {"1","2","3"}};
 
         System.out.println(str.length);
         sum2d(str);
@@ -11,7 +18,7 @@ public class Task2 {
     public static int sum2d(String[][] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < 3; j++) {
                 int val = Integer.parseInt(arr[i][j]);
                 sum += val;
             }
